2c5c7321316dfbdd0b2c4bc1d597e64ba5731553
Brian Lynch <bglynch17@gmail.com>
2018-05-25 10:59:54 +0000

    Added banned words filter

--- chatapp.py
+++ chatapp.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 # Stores messages between requests
 messages = []
 
+banned_words = ['feck', 'arse', 'girls']
+
+# rooms = {
+#     ?? : ??
+# }
+''' Creates new rooms, need dictionary '''
+# @app.route('rooms/add')
+# def add_room():
+#     roomname = request.form['roomname']
+#     rooms[roomname] = []
+#     return redirect(....)
+
 # root of our website
 @app.route("/")
 def landing_page():
@@ -21,12 +33,30 @@ def get_username():
 @app.route("/<username>")
 def get_user_page(username):
     return render_template("chat.html", the_username = username, the_messages = messages)
-#                                       
+
 
 # username/new is a dump file for the chat form
 @app.route("/<username>/new", methods=["POST"])
 def add_message(username):
     text = request.form['message']
+    # if a word in the list equals a word in the banned list,
+    # replace word with ***
+
+    words_list = text.split()
+    new_words_list = []
+    for individual_word in words_list:
+        if individual_word in banned_words:
+            new_words_list.append(len(individual_word)*'*')
+        else:
+            new_words_list.append(individual_word)
+    text = ' '.join(new_words_list)
+    
+    '''
+    Michael's method
+    words = text.split()
+    words = [ "*" * len(word) if word.lower() in banned_words else word for word in words]
+    text = " ".join(map(str,words))
+    '''
     
     message = {
         'sender':username,
